1ccd2a974e315d456c9e81146b167cb4ed9f146e
Kufreabasi <engineer_kufre@yahoo.com>
2019-09-17 20:50:03 +0100

    updated signup.js

--- js/signup.js
+++ js/signup.js
@@ -5,9 +5,34 @@ $(document).ready(function() {
         const password = $('#password').val();
         const email = $('#email').val();
         if (!fullname || !username || !password || !email) {
-            $('.errMessage').html('Please fill in missing details');
+            $('.message').html('Please fill in missing details');
             return;
           }
-
+          $.ajax({
+            method: 'GET',
+            url: `http://localhost:3000/users?email=${email}`,
+            data: {
+              email,
+            },
+            success: function(response) {
+              if (response.length) {
+                $('.message').html('User already exist');
+              } else {
+                $.ajax({
+                  method: 'POST',
+                  url: 'http://localhost:3000/users',
+                  data: {
+                    fullname,
+                    username,
+                    email,
+                    password,
+                  },
+                  success: function() {
+                    $('.message').html('Success!!!');
+                  },
+                });
+              }
+            },
+          });
     });
 });
