b2ac7560bae2782fffa9e78ecce174baadcc084f
Kufreabasi <engineer_kufre@yahoo.com>
2019-09-17 20:43:58 +0100

    updated signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-    
-})
+    $('signupSubBtn').click(function() {
+        const fullname = $('#fullname').val();
+        const username = $('#username').val();
+        const password = $('#password').val();
+        const email = $('#email').val();
+        if (!fullname || !username || !password || !email) {
+            $('.errMessage').html('Please fill in missing details');
+            return;
+          }
+
+    });
+});
