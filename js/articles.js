849389d382bba3fb30b24cc5c93d5bd6dfa2e790
Kufreabasi <engineer_kufre@yahoo.com>
2019-09-17 11:28:42 +0100

    articlejs began

---
+++ js/articles.js
@@ -0,0 +1,7 @@
+$(document).ready(function) {
+    $('.newArticleBtn').click(function() {
+        const title = $('#title').val();
+        const date = $('#date').val();
+        const text = $('#text').val();
+    })
+}
