cc4295e26d97b14f4b33564b90a065b96f6d62b5
Kufreabasi <engineer_kufre@yahoo.com>
2019-09-18 12:27:04 +0100

    adminsigninjs

--- js/signin.js
+++ js/signin.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     $('.signinbutton').click(function() {
-        var usernameSI = $('#usernameSI').val();
-        var passwordSI = $('#passwordSI').val();
+        const usernameSI = $('#usernameSI').val();
+        const passwordSI = $('#passwordSI').val();
         $.ajax({
             method: 'GET',
-            url: `http://localhost:3000/users?email=${usernameSI}&password=${passwordSI}`,
+            url: `http://localhost:3000/users?username=${usernameSI}&password=${passwordSI}`,
             data: {
                 username: usernameSI,
                 password: passwordSI,
@@ -13,7 +13,7 @@ $(document).ready(function() {
                 if (response.length) {
                     $('.signInSuccess').html('Success!!!');
                     localStorage.setItem('username', usernameSI);
-                    window.location.assign('.../ndex.html');
+                    window.location.assign('.../index.html');
                 } else {
                     $('.signInSuccess').html('Username or Password Incorrect');
                 }
