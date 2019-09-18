f13d0b03a2afb7c40c4ba38cf47edea491a11b77
Kufreabasi <engineer_kufre@yahoo.com>
2019-09-18 12:30:20 +0100

    signupjs sign_in signup adminsignup

--- js/adminsignup.js
+++ js/adminsignup.js
@@ -6,22 +6,22 @@ $(document).ready(function() {
         const password = $('#password').val();
         const email = $('#email').val();
         if (!fullname || !username || !password || !email) {
-            $('.message').html('Please fill in missing details');
+            $('.signUpSuccess').html('Please fill in missing details');
             return;
           }
           $.ajax({
             method: 'GET',
-            url: `http://localhost:3000/users?email=${email}`,
+            url: `http://localhost:3000/admin?email=${email}`,
             data: {
               email,
             },
             success: function(response) {
               if (response.length) {
-                $('.message').html('User already exist');
+                $('.signUpSuccess').html('User already exist');
               } else {
                 $.ajax({
                   method: 'POST',
-                  url: 'http://localhost:3000/users',
+                  url: 'http://localhost:3000/admin',
                   data: {
                     fullname,
                     username,
@@ -29,7 +29,7 @@ $(document).ready(function() {
                     password,
                   },
                   success: function() {
-                    $('.message').html('Success!!!');
+                    $('.signUpSuccess').html('Success!!!');
                   },
                 });
               }
